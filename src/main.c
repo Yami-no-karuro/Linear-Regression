c7a8923297ad70397c93ecf48eac86738254a73e
Yami-no-karuro <carloborgna98@gmail.com>
2025-03-27 19:52:04 +0100

    First Commit

---
+++ src/main.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float X[] = {2, 4, 6, 8};
+float Y[] = {20, 40, 60, 80};
+
+float weight = 0;
+float bias = 0;
+
+/**
+ * Computes the predicted value based on current weight and bias.
+ *
+ * @param input - The input value, X.
+ * @param weight - The current weight value, W.
+ * @param bias - The current bias value, B.
+ */
+float predict(float input, float weight, float bias) 
+{
+    return (input * weight) + bias;
+}
+
+/**
+ * Computes the MSE (mean squared error), which quantifies the difference between the actual label and the predicted value.
+ *
+ * @param input - The input value, X.
+ * @param label - The label value, Y.
+ * @param weight - The current weight value, W.
+ * @param bias - The current bias value, B.
+ */
+float cost(float input, float label, float weight, float bias)
+{
+    float predicted = predict(input, weight, bias);
+    float loss = (label - predicted) * (label - predicted);
+
+    return loss / 2;
+}
+
+/**
+ * The gradient of the weight is the partial derivative of the cost function with respect to the weight.
+ * It indicates how much the weight should be adjusted to reduce the cost.
+ *
+ * @param input - The input value, X.
+ * @param label - The label value, Y.
+ */
+float weight_grad(float input, float label) 
+{
+    float predicted = predict(input, weight, bias);
+    return (predicted - label) * input;
+}
+
+/**
+ * The gradient of the bias is the partial derivative of the cost function with respect to the bias.
+ * It indicates how much the bias should be adjusted to reduce the cost.
+ * 
+ * @param input - The input value, X.
+ * @param label - The label value, Y.
+ */
+float bias_grad(float input, float label)
+{
+    float predicted = predict(input, weight, bias);
+    return predicted - label;
+}
+
+int main() 
+{
+    int epoch = 10000;
+    float learning_rate = 1e-4;
+    int size = sizeof(X) / sizeof(X[0]);
+
+    float loss = 0;
+    float grad_w = 0;
+    float grad_b = 0;
+
+    for (size_t i = 0; i < epoch; i++) {
+        for (size_t j = 0; j < size; j++) {
+            loss = cost(X[j], Y[j], weight, bias);
+            grad_w = weight_grad(X[j], Y[j]);
+            grad_b = bias_grad(X[j], Y[j]);
+
+            weight = weight - learning_rate * grad_w;
+            bias = bias - learning_rate * grad_b;
+        }
+    }
+
+    printf("\n");
+    printf("Loss: %f \n", loss);
+    printf("Weight: %f \n", weight);
+    printf("Bias: %f \n", bias);
+
+    float input_value;
+    printf("\n");
+    printf("Insert X: ");
+    scanf("%f", &input_value);
+
+    float predicted_value = predict(input_value, weight, bias);
+    printf("Input(X) = %.2f: Predicted (Y) = %.2f\n", input_value, predicted_value);
+
+    return 0;
+}
